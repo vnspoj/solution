b4b6a05e522bed72c317bdb5a72a988b4176caf5
huynhsamha <huynhsamha@gmail.com>
2018-06-30 10:33:30 +0700

    fix code

--- src/ZABAVA.cpp
+++ src/ZABAVA.cpp
@@ -18,13 +18,15 @@ typedef unsigned long long ull;
 #define fillchar(a,x) memset(a, x, sizeof (a))
 #define faster ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-int n, m, k, a[505];
-ll  f[505][505], g[505][505];
+const int N = 505;
+int n, m, k, a[N];
+ll f[N][N], g[N][N];
 
-void solve(int x, int k, ll &ans) {
-	if (k == 1) return ;
-	ans += (ll)(x/k)*(x-x/k);
-	solve(x-x/k, k-1, ans);
+ll solve(int n, int k) {
+	int p = n/k;
+	int y = n-p*k;
+	int x = k-y;
+	return (ll)x * p*(p+1)/2 + (ll)y * (p+1) * (p+2)/2;
 }
 
 
@@ -33,30 +35,19 @@ int main() {
 //  freopen("OUT.TXT", "w", stdout);
 
 	scanf("%d%d%d", &n,&m,&k);
-	for (int i=1; i<=n; i++) {
-		int t;
-		scanf("%d", &t);
-		a[t]++;
+	FOR(i,1,n) {
+		int t; scanf("%d", &t);
+		a[t]++;	
 	}
-
-	for (int i=1; i<=m; i++) {
-		f[i][0] = (ll)a[i]*(ll)(a[i]+1)/2;
-		for (int j=1; j<=min(k, a[i]-1); j++) {
-			ll ans = 0;
-			solve(a[i], j+1, ans);
-			f[i][j] = f[i][0] - ans;
-		}
-		for (int j=a[i]; j<=k; j++) f[i][j] = f[i][a[i]-1];
+	
+	FOR(i,1,m) FOR(j,0,k) f[i][j] = solve(a[i], j+1);
+	
+	FOR(j,0,k) g[1][j] = f[1][j];
+	FOR(i,2,m) FOR(j,0,k) {
+		g[i][j] = g[i-1][j] + f[i][0];
+		FOR(c,1,j)
+			g[i][j] = min(g[i][j], g[i-1][j-c] + f[i][c]);
 	}
-
-	for (int i=0; i<=k; i++) g[1][i] = f[1][i];
-
-	for (int i=2; i<=m; i++)
-		for (int j=0; j<=k; j++) {
-			g[i][j] = g[i-1][j]+f[i][0];
-			for (int c=1; c<=j; c++)
-				g[i][j] = min(g[i][j], g[i-1][j-c]+f[i][c]);
-		}
 
 	printf("%lld", g[m][k]);
 
