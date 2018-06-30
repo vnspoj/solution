90b84d637407ddaaf0a74aeadfd87dd2fac2d328
huynhsamha <huynhsamha@gmail.com>
2018-06-30 15:18:51 +0700

    fix code

--- src/YPKTH.cpp
+++ src/YPKTH.cpp
@@ -18,19 +18,23 @@ typedef unsigned long long ull;
 #define fillchar(a,x) memset(a, x, sizeof (a))
 #define faster ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-const int   N = 100100;
-int         n, q, a[N], L, R, k, res, cnt, f;
-data        t[4*N], nil;
+typedef vector<int> data;
 
+const int N = 1e5+5;
+int n, q, a[N], L, R, k, res, cnt;
+bool found;
+data t[4*N], nil;
+
+/* combine 2 child nodes (increasing vector) to the parent node (increasing vector) */
 data combine(data u, data v) {
 	data ans = nil;
 	int i = 0, j = 0;
-	while (i < u.size() and j < v.size()) {
+	while (i < sz(u) && j < sz(v)) {
 		if (u[i] < v[j]) ans.pb(u[i++]);
 		else ans.pb(v[j++]);
 	}
-	while (i < u.size()) ans.pb(u[i++]);
-	while (j < v.size()) ans.pb(v[j++]);
+	while (i < sz(u)) ans.pb(u[i++]);
+	while (j < sz(v)) ans.pb(v[j++]);
 	return ans;
 }
 
@@ -39,32 +43,31 @@ void build(int k, int l, int r) {
 		t[k].pb(a[l]);
 		return ;
 	}
-	int mid = (l+r)/2;
-	build(k*2, l, mid);
-	build(k*2+1, mid+1, r);
+	int m = (l+r)/2;
+	build(k*2, l, m);
+	build(k*2+1, m+1, r);
 	t[k] = combine(t[k*2], t[k*2+1]);
 }
 
 void get(int node, int l, int r) {
-	if (r < L or R < l) return ;
-	if (L <= l and r <= R) {
-		int i = 0, j = t[node].size()-1, pos = -1;
+	if (r < L || R < l) return ;
+	if (L <= l && r <= R) {
+		int i = 0, j = sz(t[node])-1, pos = -1;
 		while (i <= j) {
-			int mid = (i+j)/2;
-			if (t[node][mid] <= res) {
-				pos = mid;
-				i = mid+1;
-			} else j = mid-1;
+			int m = (i+j)/2;
+			if (t[node][m] <= res) {
+				pos = m;
+				i = m+1;
+			} else j = m-1;
 		}
 		if (pos == -1) return ;
-		if (t[node][pos] == res) f = true;
 		cnt += pos + 1;
-		if (t[node][pos] == res) cnt--;
+		if (t[node][pos] == res) found = true, cnt--;
 		return ;
 	}
-	int mid = (l+r)/2;
-	get(node*2,l,mid);
-	get(node*2+1,mid+1,r);
+	int m = (l+r)/2;
+	get(node*2,l,m);
+	get(node*2+1,m+1,r);
 }
 
 int main() {
@@ -72,28 +75,29 @@ int main() {
 //  freopen("OUT.TXT", "w", stdout);
 
 	scanf("%d", &n);
-	for (int i=1; i<=n; i++) scanf("%d", &a[i]);
+	FOR(i,1,n) scanf("%d", a+i);
 
 	build(1,1,n);
 
 	scanf("%d", &q);
 	while (q--) {
-		scanf("%d%d%d", &L,&R,&k);
-		int l = 0, r = t[1].size()-1;
+		scanf("%d%d%d", &L, &R, &k);
+		int l = 0, r = sz(t[1]) - 1;
 		while (l <= r) {
-			int mid = (l+r)/2;
-			res = t[1][mid];
+			int m = (l+r)>>1;
 			cnt = 0;
-			f = 0;
+			found = false;
+			res = t[1][m];
 			get(1,1,n);
-			if (cnt == k-1 and f) {
+			if (cnt == k-1 && found) {
 				printf("%d\n", res);
 				break;
 			}
-			if (cnt < k) l = mid+1;
-			else r = mid-1;
+			if (cnt < k) l = m+1;
+			else r = m-1;
 		}
 	}
 
 	return 0;
 }
+
