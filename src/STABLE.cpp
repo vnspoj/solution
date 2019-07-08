3dd92d6f0f2485bb996f4770824f312b4ac3de55
Ha Huynh <huynhsamha@gmail.com>
2019-07-08 10:14:27 +0700

    Update STABLE.cpp

--- src/STABLE.cpp
+++ src/STABLE.cpp
@@ -9,53 +9,63 @@ typedef unsigned long long ull;
 #define X first
 #define Y second
 #define pb push_back
-#define mp make_pair
-#define ep emplace_back
 #define EL printf("\n")
 #define sz(A) (int) A.size()
 #define FOR(i,l,r) for (int i=l;i<=r;i++)
 #define FOD(i,r,l) for (int i=r;i>=l;i--)
-#define fillchar(a,x) memset(a, x, sizeof (a))
 #define faster ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-const int N = 10005;
-int n, m, s, d[N], f[N];
+const int N = 1e4+5;
+int n, m, s, len[N], cnt[N];
 vector<int> a[N];
 queue<int> st;
-map<int,bool> dd[N];
+map<ii,bool> dd;
 
-int main() {
-//	freopen("INP.TXT", "r", stdin);
-//  freopen("OUT.TXT", "w", stdout);
+int main()
+{
+    // freopen("INP.TXT", "r", stdin);
+    // freopen("OUT.TXT", "w", stdout);
 
-	scanf("%d%d%d", &n,&m,&s);
-	while (m--) {
-		int i, j;
-		scanf("%d%d", &i,&j);
-		if (!dd[i][j]) a[i].pb(j), dd[i][j] = 1;
-	}
+    faster;
 
-	d[s] = f[s] = 1;
-	st.push(s);
-	while (! st.empty()) {
-		int u = st.front();
-		st.pop();
-		for (auto v : a[u]) {
-			if (d[v] == 0) {
-				d[v] = d[u] + 1;
-				f[v] = f[u];
-				st.push(v);
-			} else {
-				if (d[v] == d[u] + 1) f[v]++;
-			}
-		}
-	}
+    cin >> n >> m >> s;
+    FOR(i,1,m) {
+        int u, v;
+        cin >> u >> v;
+        if (dd[{ u,v }]) continue; // duplicated edges
+        dd[{ u,v }] = true;
+        a[u].push_back(v);
+    }
 
-	int ans = 0;
-	FOR(i,1,n)
-	if (i != s && d[i] && f[i] > 1) ans++;
+    len[s] = 0;
+    cnt[s] = 1; 
+    // cnt == { 0: not path, 1: one path, 2: more paths }
+    // only check if more paths without counting the number of shortest paths
+    // => avoid overflow (big integer)
 
-	printf("%d\n", ans);
+    st.push(s); 
+    while (!st.empty()) {
+        int u = st.front();
+        st.pop();
+        for (int v: a[u]) {
+            if (cnt[v] == 0) { // not visited => shortest path
+                len[v] = len[u] + 1;
+                cnt[v] = cnt[u];
+                st.push(v);
+            } else { // visited
+                if (len[v] == len[u] + 1) { // more paths with the same length
+                    if (cnt[v] == 1) cnt[v] = 2;
+                }
+            }
+        }
+    }
 
-	return 0;
+    int res = 0;
+    FOR(i,1,n) {
+        res += cnt[i] == 2;
+    }
+
+    cout << res << endl;
+
+    return 0;
 }
